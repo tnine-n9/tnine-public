0412b5830cb06db302996d99a79ec44c3602cb27
tnine-n9 <3562501187@qq.com>
2026-02-27 04:23:51 +0800

    Add files via upload

---
+++ 白名单名单.lua
@@ -0,0 +1,29 @@
+{
+  "active": true,
+  "message": "1213",
+  "whitelist_users": [
+    {
+      "username": "2006_Painfulwords",
+      "add_time": "2025/11/5 20:53:27"
+    },
+    {
+      "username": "nise66669",
+      "add_time": "2025/12/8 16:47:28"
+    }
+    {
+      "username": "XXCHRISTOPHER_Deltax",
+      "add_time": "2025/12/8 16:47:28"
+    }
+    {
+      "username": "jhxmeiyoujibo",
+      "add_time": "2025/12/8 16:47:28"
+    }
+    {
+      "username": "Helalost11",
+      "add_time": "2025/12/8 16:47:28"
+    }
+  ],
+  "blocked_users": [],
+  "version": "2.0.0",
+  "last_updated": "2025-12-08"
+}
