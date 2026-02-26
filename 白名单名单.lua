c2623640726dab2c42ca7c5fc683f103dc203bb5
tnine-n9 <3562501187@qq.com>
2026-02-27 05:12:11 +0800

    Update last_updated date in 白名单名单.lua

--- 白名单名单.lua
+++ 白名单名单.lua
@@ -9,9 +9,13 @@
     {
       "username": "jhxmeiyoujibo",
       "add_time": "2025/12/8 16:47:28"
+    },
+    {
+      "username": "Helalost11",
+      "add_time": "2026/2/27 20:53:27"
     }
   ],
   "blocked_users": [],
   "version": "2.0.0",
-  "last_updated": "2025-12-08"
+  "last_updated": "2027-12-08"
 }
