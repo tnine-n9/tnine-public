4a20ff81e98ebd09c394150b72596c5838b8ace5
tnine-n9 <3562501187@qq.com>
2026-02-27 04:35:59 +0800

    Fix JSON formatting in 白名单名单.lua

--- 白名单名单.lua
+++ 白名单名单.lua
@@ -9,19 +9,19 @@
     {
       "username": "nise66669",
       "add_time": "2025/12/8 16:47:28"
-    }
+    },
     {
       "username": "XXCHRISTOPHER_Deltax",
       "add_time": "2025/12/8 16:47:28"
-    }
+    },
     {
       "username": "jhxmeiyoujibo",
       "add_time": "2025/12/8 16:47:28"
-    }
+    },
     {
       "username": "Helalost11",
       "add_time": "2025/12/8 16:47:28"
-    }
+    },
   ],
   "blocked_users": [],
   "version": "2.0.0",
