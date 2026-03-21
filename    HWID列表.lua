802e48ea5456a60323501925dace1467ce4d2be9
tnine-n9 <3562501187@qq.com>
2026-03-22 01:57:32 +0800

    Add files via upload

---
+++    HWID列表.lua
@@ -0,0 +1,402 @@
+{
+  "GtHjKlQwErTyUiOp-VexKey1": {
+    "username": "2006_Painfulwords",
+    "hwid": "13c8be0f-b264-4a52-bf2f-1a4c288c0baf"
+  },
+  "AsDfGhJkLzXcVbNm-VexKey2": {
+    "username": "",
+    "hwid": ""
+  },
+  "QwErTyUiOpAsDfGh-VexKey3": {
+    "username": "",
+    "hwid": ""
+  },
+  "ZxCvBnMmLkKjJhHg-VexKey4": {
+    "username": "",
+    "hwid": ""
+  },
+  "PoIuYtReWqAsDfGt-VexKey5": {
+    "username": "",
+    "hwid": ""
+  },
+  "HjKlQwErTyUiOpAs-VexKey6": {
+    "username": "",
+    "hwid": ""
+  },
+  "DfGhJkLzXcVbNmPo-VexKey7": {
+    "username": "",
+    "hwid": ""
+  },
+  "IyUtReWqAsDfGhJk-VexKey8": {
+    "username": "",
+    "hwid": ""
+  },
+  "LzXcVbNmPoIuYtRe-VexKey9": {
+    "username": "",
+    "hwid": ""
+  },
+  "WqAsDfGhJkLzXcVb-VexKey10": {
+    "username": "",
+    "hwid": ""
+  },
+  "NmPoIuYtReWqAsDf-VexKey11": {
+    "username": "",
+    "hwid": ""
+  },
+  "GhJkLzXcVbNmPoIu-VexKey12": {
+    "username": "",
+    "hwid": ""
+  },
+  "YtReWqAsDfGhJkLz-VexKey13": {
+    "username": "",
+    "hwid": ""
+  },
+  "XcVbNmPoIuYtReWq-VexKey14": {
+    "username": "",
+    "hwid": ""
+  },
+  "AsDfGhJkLzXcVbNmV-VexKey15": {
+    "username": "",
+    "hwid": ""
+  },
+  "QwErTyUiOpAsDfGj-VexKey16": {
+    "username": "",
+    "hwid": ""
+  },
+  "ZxCyBnMmLkKjJhHg-VexKey17": {
+    "username": "",
+    "hwid": ""
+  },
+  "PoIjYtReWqAsDfGt-VexKey18": {
+    "username": "",
+    "hwid": ""
+  },
+  "HjKlQwErTyUiOpAs-VexKey19": {
+    "username": "",
+    "hwid": ""
+  },
+  "DfGhJkLzXcVbNmPo-VexKey20": {
+    "username": "",
+    "hwid": ""
+  },
+  "IyUtReWqAsDfGhJk-VexKey21": {
+    "username": "",
+    "hwid": ""
+  },
+  "LzXcVbNmPoIuYtRe-VexKey22": {
+    "username": "",
+    "hwid": ""
+  },
+  "WqAsDfGhJkLzXcVb-VexKey23": {
+    "username": "",
+    "hwid": ""
+  },
+  "NmPoIuYtReWqAsDf-VexKey24": {
+    "username": "",
+    "hwid": ""
+  },
+  "GhJkLzXcVbNmPoIu-VexKey25": {
+    "username": "",
+    "hwid": ""
+  },
+  "YtReWqAsDfGhJkLz-VexKey26": {
+    "username": "",
+    "hwid": ""
+  },
+  "XcVbNmPoIuYtReWq-VexKey27": {
+    "username": "",
+    "hwid": ""
+  },
+  "AsDfGhJkLzXcVbNmV-VexKey28": {
+    "username": "",
+    "hwid": ""
+  },
+  "QwErTyUiOpAsDfGj-VexKey29": {
+    "username": "",
+    "hwid": ""
+  },
+  "ZxCyBnMmLkKjJhHg-VexKey30": {
+    "username": "",
+    "hwid": ""
+  },
+  "PoIjYtReWqAsDfGt-VexKey31": {
+    "username": "",
+    "hwid": ""
+  },
+  "HjKlQwErTyUiOpAs-VexKey32": {
+    "username": "",
+    "hwid": ""
+  },
+  "DfGhJkLzXcVbNmPo-VexKey33": {
+    "username": "",
+    "hwid": ""
+  },
+  "IyUtReWqAsDfGhJk-VexKey34": {
+    "username": "",
+    "hwid": ""
+  },
+  "LzXcVbNmPoIuYtRe-VexKey35": {
+    "username": "",
+    "hwid": ""
+  },
+  "WqAsDfGhJkLzXcVb-VexKey36": {
+    "username": "",
+    "hwid": ""
+  },
+  "NmPoIuYtReWqAsDf-VexKey37": {
+    "username": "",
+    "hwid": ""
+  },
+  "GhJkLzXcVbNmPoIu-VexKey38": {
+    "username": "",
+    "hwid": ""
+  },
+  "YtReWqAsDfGhJkLz-VexKey39": {
+    "username": "",
+    "hwid": ""
+  },
+  "XcVbNmPoIuYtReWq-VexKey40": {
+    "username": "",
+    "hwid": ""
+  },
+  "AsDfGhJkLzXcVbNmV-VexKey41": {
+    "username": "",
+    "hwid": ""
+  },
+  "QwErTyUiOpAsDfGj-VexKey42": {
+    "username": "",
+    "hwid": ""
+  },
+  "ZxCyBnMmLkKjJhHg-VexKey43": {
+    "username": "",
+    "hwid": ""
+  },
+  "PoIjYtReWqAsDfGt-VexKey44": {
+    "username": "",
+    "hwid": ""
+  },
+  "HjKlQwErTyUiOpAs-VexKey45": {
+    "username": "",
+    "hwid": ""
+  },
+  "DfGhJkLzXcVbNmPo-VexKey46": {
+    "username": "",
+    "hwid": ""
+  },
+  "IyUtReWqAsDfGhJk-VexKey47": {
+    "username": "",
+    "hwid": ""
+  },
+  "LzXcVbNmPoIuYtRe-VexKey48": {
+    "username": "",
+    "hwid": ""
+  },
+  "WqAsDfGhJkLzXcVb-VexKey49": {
+    "username": "",
+    "hwid": ""
+  },
+  "NmPoIuYtReWqAsDf-VexKey50": {
+    "username": "",
+    "hwid": ""
+  },
+  "GhJkLzXcVbNmPoIu-VexKey51": {
+    "username": "",
+    "hwid": ""
+  },
+  "YtReWqAsDfGhJkLz-VexKey52": {
+    "username": "",
+    "hwid": ""
+  },
+  "XcVbNmPoIuYtReWq-VexKey53": {
+    "username": "",
+    "hwid": ""
+  },
+  "AsDfGhJkLzXcVbNmV-VexKey54": {
+    "username": "",
+    "hwid": ""
+  },
+  "QwErTyUiOpAsDfGj-VexKey55": {
+    "username": "",
+    "hwid": ""
+  },
+  "ZxCyBnMmLkKjJhHg-VexKey56": {
+    "username": "",
+    "hwid": ""
+  },
+  "PoIjYtReWqAsDfGt-VexKey57": {
+    "username": "",
+    "hwid": ""
+  },
+  "HjKlQwErTyUiOpAs-VexKey58": {
+    "username": "",
+    "hwid": ""
+  },
+  "DfGhJkLzXcVbNmPo-VexKey59": {
+    "username": "",
+    "hwid": ""
+  },
+  "IyUtReWqAsDfGhJk-VexKey60": {
+    "username": "",
+    "hwid": ""
+  },
+  "LzXcVbNmPoIuYtRe-VexKey61": {
+    "username": "",
+    "hwid": ""
+  },
+  "WqAsDfGhJkLzXcVb-VexKey62": {
+    "username": "",
+    "hwid": ""
+  },
+  "NmPoIuYtReWqAsDf-VexKey63": {
+    "username": "",
+    "hwid": ""
+  },
+  "GhJkLzXcVbNmPoIu-VexKey64": {
+    "username": "",
+    "hwid": ""
+  },
+  "YtReWqAsDfGhJkLz-VexKey65": {
+    "username": "",
+    "hwid": ""
+  },
+  "XcVbNmPoIuYtReWq-VexKey66": {
+    "username": "",
+    "hwid": ""
+  },
+  "AsDfGhJkLzXcVbNmV-VexKey67": {
+    "username": "",
+    "hwid": ""
+  },
+  "QwErTyUiOpAsDfGj-VexKey68": {
+    "username": "",
+    "hwid": ""
+  },
+  "ZxCyBnMmLkKjJhHg-VexKey69": {
+    "username": "",
+    "hwid": ""
+  },
+  "PoIjYtReWqAsDfGt-VexKey70": {
+    "username": "",
+    "hwid": ""
+  },
+  "HjKlQwErTyUiOpAs-VexKey71": {
+    "username": "",
+    "hwid": ""
+  },
+  "DfGhJkLzXcVbNmPo-VexKey72": {
+    "username": "",
+    "hwid": ""
+  },
+  "IyUtReWqAsDfGhJk-VexKey73": {
+    "username": "",
+    "hwid": ""
+  },
+  "LzXcVbNmPoIuYtRe-VexKey74": {
+    "username": "",
+    "hwid": ""
+  },
+  "WqAsDfGhJkLzXcVb-VexKey75": {
+    "username": "",
+    "hwid": ""
+  },
+  "NmPoIuYtReWqAsDf-VexKey76": {
+    "username": "",
+    "hwid": ""
+  },
+  "GhJkLzXcVbNmPoIu-VexKey77": {
+    "username": "",
+    "hwid": ""
+  },
+  "YtReWqAsDfGhJkLz-VexKey78": {
+    "username": "",
+    "hwid": ""
+  },
+  "XcVbNmPoIuYtReWq-VexKey79": {
+    "username": "",
+    "hwid": ""
+  },
+  "AsDfGhJkLzXcVbNmV-VexKey80": {
+    "username": "",
+    "hwid": ""
+  },
+  "QwErTyUiOpAsDfGj-VexKey81": {
+    "username": "",
+    "hwid": ""
+  },
+  "ZxCyBnMmLkKjJhHg-VexKey82": {
+    "username": "",
+    "hwid": ""
+  },
+  "PoIjYtReWqAsDfGt-VexKey83": {
+    "username": "",
+    "hwid": ""
+  },
+  "HjKlQwErTyUiOpAs-VexKey84": {
+    "username": "",
+    "hwid": ""
+  },
+  "DfGhJkLzXcVbNmPo-VexKey85": {
+    "username": "",
+    "hwid": ""
+  },
+  "IyUtReWqAsDfGhJk-VexKey86": {
+    "username": "",
+    "hwid": ""
+  },
+  "LzXcVbNmPoIuYtRe-VexKey87": {
+    "username": "",
+    "hwid": ""
+  },
+  "WqAsDfGhJkLzXcVb-VexKey88": {
+    "username": "",
+    "hwid": ""
+  },
+  "NmPoIuYtReWqAsDf-VexKey89": {
+    "username": "",
+    "hwid": ""
+  },
+  "GhJkLzXcVbNmPoIu-VexKey90": {
+    "username": "",
+    "hwid": ""
+  },
+  "YtReWqAsDfGhJkLz-VexKey91": {
+    "username": "",
+    "hwid": ""
+  },
+  "XcVbNmPoIuYtReWq-VexKey92": {
+    "username": "",
+    "hwid": ""
+  },
+  "AsDfGhJkLzXcVbNmV-VexKey93": {
+    "username": "",
+    "hwid": ""
+  },
+  "QwErTyUiOpAsDfGj-VexKey94": {
+    "username": "",
+    "hwid": ""
+  },
+  "ZxCyBnMmLkKjJhHg-VexKey95": {
+    "username": "",
+    "hwid": ""
+  },
+  "PoIjYtReWqAsDfGt-VexKey96": {
+    "username": "",
+    "hwid": ""
+  },
+  "HjKlQwErTyUiOpAs-VexKey97": {
+    "username": "",
+    "hwid": ""
+  },
+  "DfGhJkLzXcVbNmPo-VexKey98": {
+    "username": "",
+    "hwid": ""
+  },
+  "IyUtReWqAsDfGhJk-VexKey99": {
+    "username": "",
+    "hwid": ""
+  },
+  "LzXcVbNmPoIuYtRe-VexKey100": {
+    "username": "",
+    "hwid": ""
+  }
+}
